1382916a6942bf13b44a3a959bc1c4099bece912
bristolkon <bristolkon3802@gmail.com>
2023-08-23 20:43:27 +0900

    1. type Query 지정

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`서버 url ${url}`);
+});
